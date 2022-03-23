ea58182bf4d2d6906a5dc0a3166deee60a16db98
07466621902 <joceano.borba.ext@panonline.com.br>
2022-03-22 21:27:36 -0300

    init project

---
+++ src/main/java/com/joceano/projetospringbatch/ProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.joceano.projetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBatchApplication.class, args);
+	}
+
+}
